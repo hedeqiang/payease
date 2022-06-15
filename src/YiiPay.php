d6d16442c674d76184eb5150ae936eaee3e47aa0
hedeqiang <laravel_code@163.com>
2022-06-15 13:46:36 +0800

    Support Yii2

---
+++ src/YiiPay.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the hedeqiang/payease.
+ *
+ * (c) hedeqiang <laravel_code@163.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Hedeqiang\PayEase;
+
+use yii\base\Component;
+
+class YiiPay extends Component
+{
+    /**
+     * @var array
+     */
+    public $options = [];
+
+    /**
+     * @var Pay
+     */
+    private static $_pay;
+
+    /**
+     * @return Pay
+     */
+    public function getPay(array $options = [])
+    {
+        $options and $this->options = array_merge($this->options, $options);
+        if (!static::$_pay instanceof Pay || $options) {
+            static::$_pay = new Pay($this->options);
+        }
+
+        return static::$_pay;
+    }
+
+}
---
+++ src/_ide_helper.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the hedeqiang/payease.
+ *
+ * (c) hedeqiang <laravel_code@163.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+class Yii
+{
+    /**
+     * @var Application
+     */
+    public static $app;
+}
+
+/**
+ * @property \Hedeqiang\PayEase\YiiPay $pay
+ */
+class Application extends \yii\web\Application
+{
+}
